5f31ce898ab23063f1b8984bf18b91bf186f58aa
Nick Anderson <tetramputechture@gmail.com>
2020-12-23 19:55:55 -0700

    Various fixes and polish

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
-    var possums = JSON.parse(this.responseText);
-    var possum = possums[Math.floor(Math.random() * possums.length)];
-    var possumEl = document.getElementById('opossum');
-    possumEl.src = possum.src;
-    possumEl.alt = possum.alt;
+    var opossums = JSON.parse(this.responseText);
+    var opossum = opossums[Math.floor(Math.random() * opossums.length)];
+    var opossumEl = document.getElementById('opossum');
+    opossumEl.src = opossum.src;
+    opossumEl.alt = opossum.alt;
+    opossumEl.title = opossum.alt;
   }
 };
 xmlhttp.open("GET", "https://s3.amazonaws.com/opossumblea.ch/opossums.json", true);
